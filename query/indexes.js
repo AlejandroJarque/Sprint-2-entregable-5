360f2e2483911f9a87e8699ee14e68340b1f6745
AlejandroJarque <jarquealex0302@gmail.com>
2025-10-30 17:19:40 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -1,7 +1,7 @@
-db.restaurants.createIndex({_id: 0});
-db.restaurants.createIndex({_id: 0,"restaurant_id": 1, "name": 1});
-db.restaurants.createIndex({_id: 0,"restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1});
-db.restaurants.createIndex({_id: 0,"restaurant_id": 1, "name": 1, "address.zipcode": 1});
-db.restaurants.createIndex({_id: 0,"borough": "Bronx"});
-db.restaurants.createIndex({_id: 0,"borough": "Bronx"}).limit(5);
-db.restaurants.createIndex({_id: 0,"borough": "Bronx"}).skip(5).limit(5);  
+db.restaurants.createIndex({});
+db.restaurants.createIndex({"restaurant_id": 1, "name": 1});
+db.restaurants.createIndex({"restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1});
+db.restaurants.createIndex({"restaurant_id": 1, "name": 1, "address.zipcode": 1});
+db.restaurants.createIndex({"borough": "Bronx"});
+db.restaurants.createIndex({"borough": "Bronx"}).limit(5);
+db.restaurants.createIndex({"borough": "Bronx"}).skip(5).limit(5);  
