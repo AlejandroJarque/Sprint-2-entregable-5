d71a482610634464b4f2b3ff60eb2f935fdc67ac
AlejandroJarque <jarquealex0302@gmail.com>
2025-10-30 17:14:26 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -1,1 +1,7 @@
-//Use this file to define indexes for your queries
+db.restaurants.createIndex({_id: 0});
+db.restaurants.createIndex({_id: 0,"restaurant_id": 1, "name": 1});
+db.restaurants.createIndex({_id: 0,"restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1});
+db.restaurants.createIndex({_id: 0,"restaurant_id": 1, "name": 1, "address.zipcode": 1});
+db.restaurants.createIndex({_id: 0,"borough": "Bronx"});
+db.restaurants.createIndex({_id: 0,"borough": "Bronx"}).limit(5);
+db.restaurants.createIndex({_id: 0,"borough": "Bronx"}).skip(5).limit(5);  
