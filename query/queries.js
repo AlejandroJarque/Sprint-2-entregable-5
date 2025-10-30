9f5835b1ae2ffa7fcc5f96a257e2785de6543705
AlejandroJarque <jarquealex0302@gmail.com>
2025-10-30 16:56:07 +0100

    refactoring

--- query/queries.js
+++ query/queries.js
@@ -1,20 +1,27 @@
 // 1. Mostrar tots els documents en la col·lecció Restaurants.
-{}
+db.restaurants.find(
+{})
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-{ "restaurant_id": 1, "name": 1 }
+db.restaurants.find(
+{ "restaurant_id": 1, "name": 1 })
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
-{ "restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1 }
+db.restaurants.find(
+{ "restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1 })
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
-{ "restaurant_id": 1, "name": 1, "address.zipcode": 1 }
+db.restaurants.find(
+{ "restaurant_id": 1, "name": 1, "address.zipcode": 1 })
 
 // 5. Mostrar tots els restaurants que estan en el Bronx.
-{"borough": "Bronx"}
+db.restaurants.find(
+  { "borough": "Bronx" })
 
 // 6. Mostrar els primers 5 restaurants que estan en el Bronx.
-
+db.restaurants.find(
+  { "borough": "Bronx" }   // filtro
+).limit(5)      
 
 // 7. Mostrar el pròxims 5 restaurants després de saltar els primers 5 del Bronx.
 
